cffcac09cfd5756ef2528614a07afa0b3cfb1522
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-26 14:07:45 -0300

    Finish

--- DelegatesCSharp/Program.cs
+++ DelegatesCSharp/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DelegatesCSharp.Services;
+using System;
+
 
 namespace DelegatesCSharp
 {
     internal class Program
     {
+        //as funções devem casar com a assinatura do delegate
+        delegate double BinaryNumericOperation(double n1, double n2);
+        delegate double TesteOperationWithOneArgument(double n1);
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20; 
+            BinaryNumericOperation op = CalculationService.Max;
+            TesteOperationWithOneArgument test = new TesteOperationWithOneArgument(CalculationService.Square);
+            test.Invoke(12);
+
+            Console.WriteLine(test(100));
+            Console.WriteLine(op(a, b));
+            Console.ReadKey();
         }
     }
 }
